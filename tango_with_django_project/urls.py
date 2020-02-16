4e31c4f9fa0cf08740e5fee84304da857fb9b5a6
Joumana Elshiekh <2406560e@student.gla.ac.uk>
2020-02-16 18:14:52 +0000

    chapter 4: added static and media files

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -19,9 +19,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
-    path('about/', views.about, name='about'),
-]
+    path('rango/about/', views.about, name='about'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
